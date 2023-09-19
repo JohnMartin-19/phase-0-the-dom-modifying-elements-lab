fa8f46b7d4b1090afbb5144b6f1940d3e4ccfedc
John Mburu <johnnkonge2020@gmail.com>
2023-09-19 10:02:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.innerHTML();
+
+const newHeader = document.getElementsByTagName('header');
+element.newHeader.append(<h1>Just a test</h1>);
+newHeader.idName = 'victory';
+
+const p = document.createElement('p');
+p.textContent = ('John is a champion!') 
+element.append(p);
